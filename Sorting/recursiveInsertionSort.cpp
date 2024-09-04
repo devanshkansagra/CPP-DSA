bab397c5d94dd37453f66be71fe686bcd3f9412b
devanshkansagra <dkansagra04@gmail.com>
2024-09-04 09:08:38 +0530

    recursiveInsertionSort

---
+++ Sorting/recursiveInsertionSort.cpp
@@ -0,0 +1,46 @@
+// Recursive implementation of insertion sort
+/*
+    4 1 5 2 3 
+    1 4 5 2 3 
+    1 4 5 2 3 
+    1 2 4 5 3 
+    1 2 3 4 5 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertion_sort(int arr[], int i, int n) {
+
+    // Base Case: i == n.
+    if (i == n) return;
+
+    int j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+        int temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+        j--;
+    }
+
+    insertion_sort(arr, i + 1, n);
+}
+
+int main()
+{
+    int arr[] = {4, 1, 5, 2, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Before Using insertion Sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    insertion_sort(arr, 0, n);
+    cout << "After Using insertion sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+    return 0;
+}
