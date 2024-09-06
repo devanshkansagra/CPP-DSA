1fe142ab2c19c45ec5aab42db7493b00622e0f87
devanshkansagra <dkansagra04@gmail.com>
2024-09-06 23:13:06 +0530

    Modified moveZerosEnd

--- Array/Easy/moveZerosEnd.cpp
+++ Array/Easy/moveZerosEnd.cpp
@@ -3,17 +3,31 @@
 using namespace std;
 
 void moveZeros(vector <int> &nums) {
+    vector <int> temp;
+    int n = nums.size();
     for(int i = 0; i < nums.size(); i++) {
-        if(nums[i] == 0) {
-            for(int j = i; j < nums.size(); j++) {
-                swap(nums[j], nums[j+1]);
-            }
+        if(nums[i] != 0) {
+            temp.push_back(nums[i]);
         }
+    }    
+
+    int t = temp.size();
+    nums.clear();
+
+    for(int i = 0; i < t; i++) {
+        nums.push_back(temp[i]);
     }
+
+    int i = 0;
+    while(i < n - t) {
+        nums.push_back(0);
+        i += 1;
+    }
+
 }
 int main() {
 
-    vector <int> v = {0};
+    vector <int> v = {0,1,0,3,12};
     moveZeros(v);
     for(int i = 0; i < v.size(); i++) {
         cout << v[i] << " ";
