9cbcc994b3ff9f86363e5fd21febc84f2f64f8f2
Devansh <dkansagra04@gmail.com>
2024-12-06 07:50:02 +0530

    Added knapsack dp

---
+++ DAA/knapsackDP.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int knapsack(const vector<int>& values, const vector<int>& weights, int capacity) {
+    int n = values.size();
+    vector<vector<int>> dp(n + 1, vector<int>(capacity + 1, 0));
+
+    for (int i = 1; i <= n; ++i) {
+        for (int w = 1; w <= capacity; ++w) {
+            if (weights[i - 1] <= w) {
+                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1]);
+            } else {
+                dp[i][w] = dp[i - 1][w];
+            }
+        }
+    }
+
+    return dp[n][capacity];
+}
+
+int main() {
+    vector<int> values = {60, 100, 120};
+    vector<int> weights = {10, 20, 30};
+    int capacity = 50;
+
+    cout << "Maximum value: " << knapsack(values, weights, capacity) << endl;
+    return 0;
+}
